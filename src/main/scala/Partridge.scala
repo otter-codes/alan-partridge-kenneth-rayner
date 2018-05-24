d66e0be7a240cef790d468cc8e047b4ebb082532
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-24 12:57:00 +0100

    refactor codes

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -3,12 +3,12 @@ object Partridge extends App {
   def checkForWord(inputArray: Array[String]): String = {
 
     val words = List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
-    val numExMatches = inputArray.count((x: String) => words contains x)
-    if (numExMatches == 0) {
+    val numMatches = inputArray.count((x: String) => words contains x)
+    if (numMatches == 0) {
       "Lynn, I've pierced my foot on a spike!!"
     }
     else {
-      s"Mine's a pint${"!" * numExMatches}"
+      s"Mine's a pint${"!" * numMatches}"
     }
   }
 }
