f712b33e79a22027b26e2b98c597980e825f6c38
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-24 14:22:27 +0100

    refactor code and tests

--- src/main/scala/AppleTurnover.scala
+++ src/main/scala/AppleTurnover.scala
@@ -3,7 +3,7 @@ object AppleTurnover {
   def convert (input:Any):String = {
 
     val inputToInt = input.toString.toInt
-    if  (inputToInt * inputToInt >= 2000000){
+    if  (inputToInt * inputToInt >= 1000){
       "Hotter than the sun!"
     } else "Help yourself to a honeycomb Yorkie for the glovebox"
   }
