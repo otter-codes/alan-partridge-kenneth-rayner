1c7364e5b29967fe757fa13d6fc07db64be17b13
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-24 13:11:06 +0100

    refactor code

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -3,7 +3,8 @@ object Partridge extends App {
   def checkForWord(inputArray: Array[String]): String = {
 
     val words = List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
-    val numMatches = inputArray.count((x: String) => words contains x)
+    val numMatches = inputArray.count(x => words contains x)
+    //val numMatches = words.intersect(inputArray).length
     if (numMatches == 0) {
       "Lynn, I've pierced my foot on a spike!!"
     }
