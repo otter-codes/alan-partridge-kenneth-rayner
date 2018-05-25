5560df520a991272a78affc919b4ad629bc81de2
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-25 09:31:10 +0100

    new code and test

--- src/main/scala/Stations.scala
+++ src/main/scala/Stations.scala
@@ -2,8 +2,8 @@ object Stations {
 
   def stopChecker (input: List[String]):String = {
     val stationsRequired = List("Rejection","Disappointment","Backstabbing Central","Shattered Dreams Parkway")
-    val numMatches = stationsRequired.intersect(input).length
-    if (numMatches < stationsRequired.length) {
+    val numMatches = stationsRequired.intersect(input)
+    if (numMatches.toSet != stationsRequired.toSet){
       "No, seriously, run. You will miss it."
     }
     else {
